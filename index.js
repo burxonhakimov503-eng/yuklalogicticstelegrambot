df533bd550edcb5812ad5eb1e7530dbbe013086a
burxonhakimov503-eng <burxonhakimov503@gmail.com>
2026-04-29 17:55:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -745,3 +745,11 @@ bot.hears('📱 Mini App', (ctx) => {
         [Markup.button.webApp('📱 Ochish', MINI_APP_URL)]
     ]));
 });
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'miniapp.html'));
+});
+app.listen(PORT, () => console.log(`Server: ${PORT}`));
